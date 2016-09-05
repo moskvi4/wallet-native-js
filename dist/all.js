79667ac728effb1fe686754f72705769bd83a394
A.Zhyvchyn <zhyvchyn932@gmail.com>
2016-09-05 18:56:59 +0300

    modified gulp

--- dist/all.js
+++ dist/all.js
@@ -6,3 +6,72 @@ wallet.view = wallet.view || {};
 wallet.controller = wallet.controller || {};
 wallet.helper = wallet.helper || {};
 wallet.template = wallet.template || {};
+wallet.helper.constants = {
+	INCOMING: 1,
+	OUTGOING: 2
+};
+
+wallet.helper.isFunction = function(functionToCheck) {
+	var getType = {};
+	return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+};
+wallet.model.Cashflow = function(amount, dest, desc) {
+	amount = amount || 0;
+	amount = Math.abs(amount);
+	this._dest = dest || wallet.helper.constants.INCOMING;
+	if (this._dest === wallet.helper.constants.OUTGOING) {
+		this._amount = -amount;
+	} else {
+		this._amount = amount;
+	}
+	this._desc = desc || '';
+};
+
+wallet.model.Cashflow.prototype = {
+	amount: function(amount) {
+		if (amount === undefined) {
+			return this._amount;
+		}
+		this._amount = amount;
+		this.update();
+	},
+
+	destination: function(destination) {
+		if (destination === undefined) {
+			return this._dest;
+		}
+		this._dest = destination;
+		this.update();
+	},
+
+	description: function(description) {
+		if (description === undefined) {
+			return this._desc;
+		}
+		this._desc = description;
+	},
+
+	update: function(args) {
+		args = args || [this._amount, this._dest];
+		if (wallet.helper.isFunction(this.onUpdate)) {
+			this.onUpdate(args);
+		}
+	},
+
+	destroy: function() {
+		if (wallet.helper.isFunction(this.onDestroy)) {
+			this.onDestroy();
+		}
+		this.onDestroy = null;
+		this.onUpdate = null;
+	},
+
+	onDestroy: null,
+
+	onUpdate: null
+};
+
+wallet.template.Cashflow =
+		'<div class="cashflow-view">' +
+		'</div>'
+;
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('default', ['build-js', 'bild-css']);
+gulp.task('default', ['build-js', 'build-css']);
 
-gulp.task('build-js', function () {
-    return gulp.src('src/js/namespace.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist/'));
+gulp.task('build-js', function() {
+	return gulp.src('src/js/*.js')
+			.pipe(concat('all.js'))
+			.pipe(gulp.dest('dist/'));
 });
 
-gulp.task('bild-css', function () {
-    return gulp.src('src/css/*.css')
-        .pipe(gulp.dest('dist/'));
+gulp.task('build-css', function() {
+	return gulp.src('src/css/*.css')
+			.pipe(gulp.dest('dist/'));
 });
